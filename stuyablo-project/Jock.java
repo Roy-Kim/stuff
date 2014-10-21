4ee9a01a4198538b8289f43d51a8c31d7e2359ab
Roy-Kim <royjkim0519@gmail.com>
2014-10-21 15:25:18 -0400

    Create Jock.java

---
+++ stuyablo-project/Jock.java
@@ -0,0 +1,16 @@
+public class Jock extends Character{
+	public Jock(){
+		name = "Braaaaah";
+		health = 20;
+		defense = 15;
+		intelligence = 1;
+		attack = 10;
+	}
+	public Jock(String n){
+		name = n
+		health = 20;
+		defense = 15;
+		intelligence = 1;
+		attack = 10;
+	}
+}
